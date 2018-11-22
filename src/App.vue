37f0466310999c3042ac7eabf18a385579581e2b
jimocc <245629490@qq.com>
2018-11-22 11:11:11 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
     <div id="app">
-        
+
         <router-view />
         <!-- tabbar -->
         <van-tabbar v-model="active">
-            <van-tabbar-item icon="shop">首页</van-tabbar-item>
+            <van-tabbar-item icon="shop" to='/mall'>首页</van-tabbar-item>
             <van-tabbar-item icon="more-o">标签</van-tabbar-item>
-            <van-tabbar-item icon="cart">购物车</van-tabbar-item>
+            <van-tabbar-item icon="cart" to='/cart'>购物车</van-tabbar-item>
             <van-tabbar-item icon="pending-orders">订单</van-tabbar-item>
         </van-tabbar>
 
@@ -20,7 +20,11 @@ export default {
         [Tabbar.name]: Tabbar,
         [TabbarItem.name]: TabbarItem
     },
-    data() { },
+    data() {
+        return {
+            active: 0
+        }
+    },
     mounted() { },
     methods: {}
 };
