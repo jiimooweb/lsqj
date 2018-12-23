b6f45a324eff627a0da15aac8767336325ef3aa8
dongfang <1163129165@qq.com>
2018-12-23 22:24:31 +0800

    更新抽奖界面

--- src/views/lottery/index.vue
+++ src/views/lottery/index.vue
@@ -2,21 +2,28 @@
     <div>
         <div class="turnPage">
             <img class="turnPic" :src="turn_img">
-            <img class="pointPic" :src="pointer_img" @click="getResult()">
+            <img :class="'pointPic ' + (rotateClass?'rotate':'')" :style="'transform: rotate('+this.deg+'deg)'" :src="pointer_img" @click="getResult()">
         </div>
     </div>
 </template>
 
 <script>
+import {Dialog} from "vant";
 import axios from "../../public/axios.js";
 import Token from "../../public/util.js";
 const token = new Token();
 export default {
+    components: {
+        [Dialog.name]: Dialog,
+    },
     data() {
         return {
             turn_img:'',
             pointer_img:'',
-            lotteryData:{}
+            lotteryData:{},
+            rotateClass:false,
+            deg:0,
+            isTurn:false,
         };
     },
     methods:{
@@ -32,15 +39,32 @@ export default {
             })
         },
         getResult(){
-            axios.request({
-                url:'lottery/result',
-                method:'post',
-                data:{
-                    id:1
-                }
-            }).then(res=>{
-                console.log(res);
-            })
+            if(this.isTurn){
+                return false
+            }
+            this.isTurn = true
+            this.rotateClass = false
+            this.deg = this.deg - 360*10
+            // if(!this.rotateClass){
+                axios.request({
+                    url:'lottery/result',
+                    method:'post',
+                    data:{
+                        activity_id:1
+                    }
+                }).then(res=>{
+                    this.rotateClass = true
+                    this.deg = res.data.result_prize.result_angle + 360*10
+                    setTimeout(()=>{
+                        Dialog.alert({
+                            message: "恭喜获得’"+res.data.result_prize.coupon.name+"‘"
+                        }).then(() => {
+                            // on close
+                        });
+                        this.isTurn = false
+                    },3000)
+                })
+            // }
         }
     },
     mounted() {
@@ -55,11 +79,12 @@ export default {
     width: 100%;
     height: 100%;
     position: absolute;
-    background: #ffcc00;
+    background: url('../../assets/lotteryBg.jpg') no-repeat center 0 #fe2045;
+    background-size: 100% auto;
     .turnPic{
         width: 80%;
         display: block;
-        margin: 30% auto 0;
+        margin: 55% auto 0;
     }
     .pointPic{
         width: 40%;
@@ -67,4 +92,20 @@ export default {
         margin: -60% auto 0;
     }
 }
+.rotate{
+    transition: all 3s;
+    // animation:rotatePic 2s linear 0.2s infinite;
+}
+.van-dialog__message{
+    text-align: center;
+}
+@keyframes rotatePic{
+    0%{
+        transform: rotate(0deg);
+    }
+    100%{
+        transform: rotate(2880deg);
+    }
+}
+
 </style>
